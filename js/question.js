b518439602087d1e31a5c76851c8933a24a81ec1
Oleg Polyakov <olegpolyako@outlook.com>
2018-05-16 12:06:47 +0300

    Update questions

--- js/question.js
+++ js/question.js
@@ -4,7 +4,8 @@ export default class Question {
      * @param {string[]} answers Варианты ответов
      * @param {number} correctAnswer Индекс правильного ответа
      */
-    constructor(text, answers, correctAnswer) {
+    constructor(type, text, answers, correctAnswer) {
+        this.type = type;
         this.text = text;
         this.answers = answers;
         this.correctAnswer = correctAnswer;
